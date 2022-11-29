6df74efef8e94d3d22408ceda7e866290200bed1
jeff ndungu <ndungujeff.ke@gmail.com>
2022-11-30 01:11:54 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const main = document.querySelector('main#main')
+main.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = `<h1>Jeff is the champion</h1>`
